abe9cc7bc397725d36d84958ce774306310aedcc
Daniil Lebedev <daniilwork247@gmail.com>
2021-03-22 15:32:16 +0000

    Added a model and page with passwords

--- passwordgeneratorproject/passwordgeneratorapp/models.py
+++ passwordgeneratorproject/passwordgeneratorapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+"""model for Password"""
+class Password(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=300)
+    password = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.name
+
--- passwordgeneratorproject/passwordgeneratorapp/views.py
+++ passwordgeneratorproject/passwordgeneratorapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Password
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    all_passwords = Password.objects.all()
+    return render(request, 'index.html', 
+    {'all_passwords': all_passwords})
