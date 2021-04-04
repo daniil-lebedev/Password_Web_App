9af1841f964e79dd24c1f00e7605dcae26c95e54
Daniil Lebedev <daniilwork247@gmail.com>
2021-04-04 21:06:46 +0100

    Just added some minor comments.

--- passwordgeneratorproject/passwordgeneratorapp/urls.py
+++ passwordgeneratorproject/passwordgeneratorapp/urls.py
@@ -4,12 +4,15 @@ from passwordgeneratorapp.views import GeneratePasswordView, CheckPassword
 
 
 urlpatterns = [
+	#path for initial stuff
     path('', views.index, name="index"),
     path('add', views.add, name='add'),
 
+    #path for password creation and check
     path('delete/<password_id>', views.passwordDelete, name="delete"),
     path('passwordgenerator', GeneratePasswordView.as_view(), name="passwordgenerator"),
     path('passwordchecker', CheckPassword.as_view(), name='passwordchecker'),
 
+    #path for register page
     path('register', views.createuser, name="register")
 ]
